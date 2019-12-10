e4275d07ede31b3a37a3f5c8cedb75035519e446
KwekerOats <46407950+KwekerOats@users.noreply.github.com>
2019-12-10 13:17:46 +0000

    Add files via upload

--- ddrgame.py
+++ ddrgame.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import pygame
 from pygame.locals import *
-import os
+import random
 import time
 
 os.environ['SDL_VIDEO_CENTERED'] = "True"
@@ -13,19 +13,31 @@ screen=pygame.display.set_mode((width, height))
 
 keys = [False,False,False,False]
 
-colour = (255,0,0)
+colour = (0,0,255)
 size = 50
 startan = 10
 
 llsquare,lmsquare,rmsquare,rrsquare = colour,colour,colour,colour
 
-def hit_check(timer,lltimes):
-    for x in happening:
+def hit_check(timer,lltimes,lmtimes,rmtimes,rrtimes):
+    for x in llhappening:
+        if timer - .5 <= x <= timer + .5:
+            return True
+    for x in lmhappening:
+        if timer - .5 <= x <= timer + .5:
+            return True
+    for x in rmhappening:
+        if timer - .5 <= x <= timer + .5:
+            return True
+    for x in rrhappening:
         if timer - .5 <= x <= timer + .5:
             return True
 
 def hit_animation():
     pygame.draw.rect(screen,llsquare,(width/4 - 100,height/20,size + 10,size + 10))
+    pygame.draw.rect(screen,llsquare,(width/2 - 100,height/20,size + 10,size + 10))
+    pygame.draw.rect(screen,llsquare,(width*.75 - 100,height/20,size + 10,size + 10))
+    pygame.draw.rect(screen,llsquare,(width - 100,height/20,size + 10,size + 10))
 
 
 start_time = time.time()
@@ -40,9 +52,7 @@ while True:
             exit(0)
 
         if event.type == KEYDOWN and event.key == K_LEFT:
-            print(timer)
-            print(happening)
-            if hit_check(timer,lltimes):
+            if hit_check(timer,lltimes,lmtimes,rmtimes,rrtimes):
                 startan = 0
 
     screen.fill(0)
@@ -53,7 +63,7 @@ while True:
     pygame.draw.rect(screen,rrsquare,(width - 100,height/20,size,size))
 
     if startan < 10:
-        llsquare = (0,255,0)
+        llsquare,lmsquare,rmsquare,rrsquare = (0,255,0),(0,255,0),(0,255,0),(0,255,0)
         hit_animation()
     
     pygame.draw.rect(screen, (255,255,255), (width/2 - 5, 0, 10, height))
@@ -62,13 +72,44 @@ while True:
     pygame.draw.rect(screen, (255,255,255), (width - 5, 0, 10, height))
     pygame.draw.rect(screen, (255,255,255), (-5, 0, 10, height))
 
-    lltimes = [1, 4, 5, 7]
-    happening = [x for x in lltimes if x <= timer]
-    for x in happening:
+    #lltimes = [random.randint(2,20) for x in range(5)]
+    lltimes = [6,9,10,18]
+    llhappening = [x for x in lltimes if x <= timer]
+    for x in llhappening:
         lltimes.remove(x)
 
     for x in range(len(lltimes)):
         pygame.draw.rect(screen,(255,255,255),(width/4 - 100,(lltimes[x] - timer)*100 + height/20,size,size))
+
+
+    #lmtimes = [random.randint(2,20) for x in range(5)]
+    lmtimes = [2,5,7,9]
+    lmhappening = [x for x in lmtimes if x <= timer]
+    for x in lmhappening:
+        lmtimes.remove(x)
+
+    for x in range(len(lmtimes)):
+        pygame.draw.rect(screen,(255,255,255),(width/2 - 100,(lmtimes[x] - timer)*100 + height/20,size,size))
+
+
+    #rmtimes = [random.randint(2,20) for x in range(5)]
+    rmtimes = [1,3,13,17]
+    rmhappening = [x for x in rmtimes if x <= timer]
+    for x in rmhappening:
+        rmtimes.remove(x)
+
+    for x in range(len(rmtimes)):
+        pygame.draw.rect(screen,(255,255,255),(width*.75 - 100,(rmtimes[x] - timer)*100 + height/20,size,size))
+
+
+    #rrtimes = [random.randint(2,20) for x in range(5)]
+    rrtimes = [1,3,10,12]
+    rrhappening = [x for x in rrtimes if x <= timer]
+    for x in rrhappening:
+        rrtimes.remove(x)
+
+    for x in range(len(rrtimes)):
+        pygame.draw.rect(screen,(255,255,255),(width - 100,(rrtimes[x] - timer)*100 + height/20,size,size))
 
     font = pygame.font.Font('Road_Rage.ttf',40)
     text = font.render(str(timer), True, (255,100,200),(0))
